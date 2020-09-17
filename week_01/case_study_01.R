fce505d2eff35d7e189742fa273d9be23accafe0
Keenan Phillips <keenanph@buffalo.edu>
2020-09-17 16:04:03 -0400

    9/17/2020 Updating the file to reflect the script created in the first two case studies.

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,1 +1,7 @@
 data(iris)
+library(ggplot2)
+data("iris")
+petal_length_mean <- mean(iris$Petal.Length)
+(petal_length_mean)
+ggplot(data=iris,aes(Petal.Length))+
+  geom_histogram()
--- week_02/case_study_02.R
+++ week_02/case_study_02.R
@@ -1,1 +1,19 @@
- 
+#Keenan Phillips
+#Spatial Data Science
+#My grandfather says climate is cooling
+
+library(tidyverse)
+
+data_url <- 'c:/Users/Baloo/Desktop/station.csv'
+temp <- read_csv(data_url, skip=1,na='999.90',
+                 col_names = c('YEAR','JAN','FEB','MAR','APR','MAY',
+                               'JUN','JUL','AUG','SEP','OCT','NOV','DEC','DJF',
+                               'MAM','JJA','SON','metANN'))
+
+plot <- ggplot(temp,aes(YEAR,JJA))+geom_line()+geom_smooth(color='red')
+
+plot+labs(title='Mean Summer Temperatures in Buffalo,NY',subtitle = 
+            'Summer includes June, July, and August
+Data from the Global Historical Climate Network',caption= 'Red line is a LOESS smooth',
+          x='Year',y='Mean Summer Temperatures (C)')
+ggsave('Mean_temp_JJA.png')
